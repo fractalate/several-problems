90a507e4c082d0d6e545c5b48558fb107d631a4f
Kate Novak <kje@cochleoid.com>
2025-09-20 14:19:45 -0400

    AOC-2022-01A Solution

---
+++ AOC-2022-01A.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+  let mut total: i64 = 0;
+  let mut maximum: i64 = 0;
+
+  let stdin = io::stdin();
+  let mut lines = stdin.lines();
+  while let Some(Ok(line)) = lines.next() { // TODO: combine the no-thing-found condition action with the post-loop action
+    if line.len() == 0 {
+      if total > maximum {
+        maximum = total;
+      }
+      total = 0;
+    } else {
+      let v = line.parse::<i64>().unwrap();
+      total += v;
+    }
+  } 
+  if total > maximum {
+    maximum = total;
+  }
+  println!("{}", maximum);
+}
