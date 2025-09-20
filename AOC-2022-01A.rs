cf6e0ced04ba3ded7c6ca3bb6f89edd10aeb2e7c
Kate Novak <kje@cochleoid.com>
2025-09-20 14:35:22 -0400

    AOC-2022-01A Cleanup

--- AOC-2022-01A.rs
+++ AOC-2022-01A.rs
@@ -1,11 +1,12 @@
 use std::io;
+use std::io::BufRead;
 
 fn main() {
   let mut total: i64 = 0;
   let mut maximum: i64 = 0;
 
   let stdin = io::stdin();
-  let mut lines = stdin.lines();
+  let mut lines = stdin.lock().lines();
   while let Some(Ok(line)) = lines.next() { // TODO: combine the no-thing-found condition action with the post-loop action
     if line.len() == 0 {
       if total > maximum {
