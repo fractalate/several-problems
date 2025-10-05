659fc97d98dcfe8e195a2591fcb645f53204b911
Kate Novak <kje@cochleoid.com>
2025-10-05 09:32:57 -0400

    UVA-10000

---
+++ UVA-10000.rs
@@ -0,0 +1,133 @@
+// UVA-10000 - Longest Path
+//
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=12&page=show_problem&problem=941
+//
+// We're given a connected, directed graph with no cycles and we want to find the longest
+// path starting at a given node.
+
+use std::io::{
+  self,
+  BufRead,
+  Lines,
+  StdinLock,
+};
+
+#[derive(Debug)]
+enum ReadProblemError {
+  NoMoreLines,
+  InvalidFormat,
+  ParseError,
+}
+
+fn read_usize(lines: &mut Lines<StdinLock<'static>>) -> Result<usize, ReadProblemError> {
+  if let Some(Ok(line)) = lines.next() {
+    return line.parse::<usize>().map_err(|_| ReadProblemError::ParseError);
+  }
+  return Err(ReadProblemError::NoMoreLines);
+}
+
+fn read_usize_pair(lines: &mut Lines<StdinLock<'static>>) -> Result<(usize, usize), ReadProblemError> {
+  if let Some(Ok(line)) = lines.next() {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    if parts.len() != 2 {
+      return Err(ReadProblemError::InvalidFormat);
+    }
+    let left_value = parts[0].parse::<usize>().map_err(|_| ReadProblemError::ParseError)?;
+    let right_value = parts[1].parse::<usize>().map_err(|_| ReadProblemError::ParseError)?;
+    return Ok((left_value, right_value));
+  }
+  return Err(ReadProblemError::NoMoreLines);
+}
+
+#[derive(Clone)]
+struct GraphNode {
+  seen: bool,
+  depth: usize,
+}
+
+struct Problem {
+  count_nodes: usize,
+  start_node: usize,
+  edges: Vec<Vec<usize>>,
+}
+
+fn read_problem(lines: &mut Lines<StdinLock<'static>>) -> Result<Option<Problem>, ReadProblemError> {
+  let count_nodes = read_usize(lines)?;
+
+  if count_nodes == 0 {
+    return Ok(None);
+  }
+
+  let start_node = read_usize(lines)?;
+  let mut edges: Vec<Vec<usize>> = vec![Vec::new(); count_nodes];
+
+  loop {
+    let (parent_node, child_node) = read_usize_pair(lines)?;
+    if parent_node == 0 || child_node == 0 {
+      break;
+    }
+    edges[parent_node - 1].push(child_node - 1);
+  }
+
+  return Ok(Some(Problem {
+    count_nodes,
+    start_node: start_node - 1,
+    edges,
+  }));
+}
+
+fn print_solution(case_number: usize, problem: &Problem) {
+  let mut nodes = vec![
+    GraphNode {
+      seen: false,
+      depth: 0,
+    };
+    problem.count_nodes
+  ];
+  let mut stack: Vec<usize> = Vec::from([problem.start_node]);
+  let mut maximum_depth = 0;
+  let mut maximum_depth_node = problem.start_node;
+
+  while let Some(parent_node) = stack.pop() {
+    if nodes[parent_node].seen {
+      continue;
+    }
+
+    for child_node in &problem.edges[parent_node] {
+      let next_depth = nodes[*child_node].depth.max(
+        nodes[parent_node].depth + 1,
+      );
+      nodes[*child_node].depth = next_depth;
+
+      if next_depth > maximum_depth || (next_depth == maximum_depth && *child_node < maximum_depth_node) {
+        maximum_depth = next_depth;
+        maximum_depth_node = *child_node;
+      }
+
+      stack.push(*child_node);
+    }
+
+    nodes[parent_node].seen = true;
+  }
+
+  println!(
+    "Case {}: The longest path from {} has length {}, finishing at {}.",
+    case_number,
+    problem.start_node + 1,
+    maximum_depth,
+    maximum_depth_node + 1,
+  );
+}
+
+fn main() -> Result<(), ReadProblemError> {
+  let stdin = io::stdin();
+  let mut lines = stdin.lock().lines();
+  let mut case_number: usize = 1;
+
+  while let Some(problem) = read_problem(&mut lines)? {
+    print_solution(case_number, &problem);
+    case_number += 1;
+  }
+
+  return Ok(());
+}
