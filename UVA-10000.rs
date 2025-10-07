3751ce79d07e9659068831cac66b06bf34e8b1b3
Kate Novak <kje@cochleoid.com>
2025-10-06 22:09:13 -0400

    Kahn-like Algorithm for UVA-10000

--- UVA-10000.rs
+++ UVA-10000.rs
@@ -5,6 +5,9 @@
 // We're given a connected, directed graph with no cycles and we want to find the longest
 // path starting at a given node.
 
+use std::collections::{
+  HashSet,
+};
 use std::io::{
   self,
   BufRead,
@@ -39,16 +42,11 @@ fn read_usize_pair(lines: &mut Lines<StdinLock<'static>>) -> Result<(usize, usiz
   return Err(ReadProblemError::NoMoreLines);
 }
 
-#[derive(Clone)]
-struct GraphNode {
-  seen: bool,
-  depth: usize,
-}
-
 struct Problem {
   count_nodes: usize,
   start_node: usize,
-  edges: Vec<Vec<usize>>,
+  edges_out: Vec<HashSet<usize>>,
+  edges_in: Vec<HashSet<usize>>,
 }
 
 fn read_problem(lines: &mut Lines<StdinLock<'static>>) -> Result<Option<Problem>, ReadProblemError> {
@@ -59,55 +57,74 @@ fn read_problem(lines: &mut Lines<StdinLock<'static>>) -> Result<Option<Problem>
   }
 
   let start_node = read_usize(lines)?;
-  let mut edges: Vec<Vec<usize>> = vec![Vec::new(); count_nodes];
+  let mut edges_out: Vec<HashSet<usize>> = vec![HashSet::new(); count_nodes];
+  let mut edges_in: Vec<HashSet<usize>> = vec![HashSet::new(); count_nodes];
 
   loop {
     let (parent_node, child_node) = read_usize_pair(lines)?;
     if parent_node == 0 || child_node == 0 {
       break;
     }
-    edges[parent_node - 1].push(child_node - 1);
+    edges_out[parent_node - 1].insert(child_node - 1);
+    edges_in[child_node - 1].insert(parent_node - 1);
   }
 
   return Ok(Some(Problem {
     count_nodes,
     start_node: start_node - 1,
-    edges,
+    edges_out,
+    edges_in,
   }));
 }
 
+#[derive(Clone)]
+struct GraphNode {
+  degree_in: usize,
+  depth: Option<usize>,
+}
+
 fn print_solution(case_number: usize, problem: &Problem) {
-  let mut nodes = vec![
-    GraphNode {
-      seen: false,
-      depth: 0,
+  let mut stack: Vec<usize> = Vec::new();
+  let mut nodes: Vec<GraphNode> = Vec::new();
+  nodes.reserve(problem.count_nodes);
+
+  for node in 0..problem.count_nodes {
+    let degree_in = problem.edges_in[node].len();
+    let depth = if node == problem.start_node {
+      Some(0)
+    } else {
+      None
     };
-    problem.count_nodes
-  ];
-  let mut stack: Vec<usize> = Vec::from([problem.start_node]);
+
+    nodes.push(GraphNode {
+      degree_in,
+      depth,
+    });
+
+    if degree_in == 0 {
+      stack.push(node);
+    }
+  }
+
   let mut maximum_depth = 0;
   let mut maximum_depth_node = problem.start_node;
 
   while let Some(parent_node) = stack.pop() {
-    if nodes[parent_node].seen {
-      continue;
-    }
-
-    for child_node in &problem.edges[parent_node] {
-      let next_depth = nodes[*child_node].depth.max(
-        nodes[parent_node].depth + 1,
-      );
-      nodes[*child_node].depth = next_depth;
-
-      if next_depth > maximum_depth || (next_depth == maximum_depth && *child_node < maximum_depth_node) {
-        maximum_depth = next_depth;
-        maximum_depth_node = *child_node;
+    for child_node in &problem.edges_out[parent_node] {
+      if let Some(depth) = nodes[parent_node].depth {
+        let next_depth = nodes[*child_node].depth.unwrap_or(0).max(depth + 1);
+        if next_depth > maximum_depth || (next_depth == maximum_depth && *child_node < maximum_depth_node) {
+          maximum_depth = next_depth;
+          maximum_depth_node = *child_node;
+        }
+        nodes[*child_node].depth = Some(next_depth);
       }
 
-      stack.push(*child_node);
+      nodes[*child_node].degree_in -= 1;
+      if nodes[*child_node].degree_in == 0 {
+        stack.push(*child_node);
+      }
     }
-
-    nodes[parent_node].seen = true;
   }
 
   println!(
