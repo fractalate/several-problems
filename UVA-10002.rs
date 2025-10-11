72cd34f1a35475f299edba982e64a17bc5d6380a
Kate Novak <kje@cochleoid.com>
2025-10-11 13:10:17 -0400

    UVA-10002 Solution

--- UVA-10002.rs
+++ UVA-10002.rs
@@ -1,4 +1,9 @@
 // https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=12&page=show_problem&problem=943
+//
+// todo: inputs are going to be integers, we might be able to avoid some corner cases where
+// floating point introduces them if we wait to convert to float as long as possible.
+//
+// 
 
 use std::io::{self, Read, Result, StdinLock};
 
@@ -62,7 +67,7 @@ fn read_problem(words: &mut WordsIterator) -> Result<Option<Problem>> {
     if problem_size >= 3 {
       let mut points: Vec<(f64, f64)> = Vec::new();
 
-      for i in 0..problem_size {
+      for _ in 0..problem_size {
         let x: f64 = if let Some(Ok(x)) = words.next() {
           x.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid coordinate"))?
         } else {
@@ -87,12 +92,75 @@ fn read_problem(words: &mut WordsIterator) -> Result<Option<Problem>> {
   return Ok(None);
 }
 
+fn print_solution(mut problem: Problem) {
+  let mut px = problem.points[0].0;
+  let mut py = problem.points[0].1;
+  for (x, y) in (&problem.points).into_iter().skip(1) {
+    if *y < py || (*y == py && *x < px) {
+      px = *x;
+      py = *y;
+    }
+  }
+
+  problem.points.sort_by(|(ax, ay), (bx, by)| {
+    let dxa = ax - px;
+    let dya = ay - py;
+    let dxb = bx - px;
+    let dyb = by - py;
+
+    let ma = if dxa == 0.0 && dya == 0.0 { 0.0 } else { dya / dxa };
+    let mb = if dxb == 0.0 && dyb == 0.0 { 0.0 } else { dyb / dxb };
+
+    if ma < mb {
+      return std::cmp::Ordering::Less;
+    } else if ma > mb {
+      return std::cmp::Ordering::Greater;
+    } else if dxa + dya < dxb + dyb {
+      return std::cmp::Ordering::Less;
+    } else if dxa + dya > dxb + dyb {
+      return std::cmp::Ordering::Greater;
+    }
+
+    return std::cmp::Ordering::Equal;
+  });
+
+  let (rx, ry) = problem.points[0];
+
+  let mut tx = 0.0;
+  let mut ty = 0.0;
+  let mut ta = 0.0;
+
+  for i in 1..problem.points.len() - 1 {
+    let (ax, ay) = problem.points[i];
+    let (bx, by) = problem.points[i + 1];
+
+    let (x, y) = center_of_triangle(ax, ay, bx, by, rx, ry);
+    let area = area_of_triangle(ax, ay, bx, by, rx, ry);
+
+    tx += x * area;
+    ty += y * area;
+    ta += area;
+  }
+
+  let (sx, sy) = (tx / ta, ty / ta);
+
+  println!("{sx:.3} {sy:.3}");
+}
+
+fn center_of_triangle(ax: f64, ay: f64, bx: f64, by: f64, cx: f64, cy: f64) -> (f64, f64) {
+  return ((ax + bx + cx) / 3.0, (ay + by + cy) / 3.0);
+}
+
+fn area_of_triangle(ax: f64, ay: f64, bx: f64, by: f64, cx: f64, cy: f64) -> f64 {
+  return ((ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) / 2.0).abs();
+}
+
 fn main() -> Result<()> {
   let mut stdin = io::stdin().lock();
   let mut words = WordsIterator::new(&mut stdin);
 
   while let Some(problem) = read_problem(&mut words)? {
-    println!("READ ONE {}", problem.points.len());
+    print_solution(problem);
   }
 
   return Ok(());
