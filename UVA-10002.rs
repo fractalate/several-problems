61debd8c8f8b239b69dffff9a26a5d290cd6d547
Kate Novak <kje@cochleoid.com>
2025-10-12 14:48:43 -0400

    Less Clumsy Input for UVA-10002

--- UVA-10002.rs
+++ UVA-10002.rs
@@ -5,24 +5,35 @@
 //
 // 
 
-use std::io::{self, Read, Result, StdinLock};
+use std::{collections::VecDeque, io::{self, BufRead, Lines, Result, StdinLock}};
 
 // Maybe some day we don't just read the whole input, but instead read portions and produce the words.
 struct WordsIterator<'a> {
-  stdin: &'a mut StdinLock<'static>,
-  has_read: bool,
-  words: Vec<String>,
-  index: usize,
+  lines: &'a mut Lines<StdinLock<'static>>,
+  words: VecDeque<String>,
 }
 
 impl<'a> WordsIterator<'a> {
-  fn new(stdin: &'a mut StdinLock<'static>) -> WordsIterator<'a> {
+  fn new(lines: &'a mut Lines<StdinLock<'static>>) -> WordsIterator<'a> {
     WordsIterator {
-      stdin,
-      has_read: false,
-      words: Vec::new(),
-      index: 0,
+      lines,
+      words: VecDeque::new(),
     }
+  }
+
+  fn read_words(&mut self) -> Result<()> {
+    let result = self.lines.next();
+
+    if let Some(Err(x)) = result {
+      return Err(x);
+    }
+
+    if let Some(Ok(line)) = result {
+      let parts = line.split_whitespace().filter(|x| x.len() > 0).map(|x| x.into());
+      self.words.extend(parts);
+    }
+
+    return Ok(());
   }
 }
 
@@ -30,26 +41,16 @@ impl<'a> Iterator for WordsIterator<'a> {
   type Item = Result<String>;
 
   fn next(&mut self) -> Option<Result<String>> {
-    if !self.has_read {
-      let mut buf: Vec<u8> = Vec::new();
-      
-      if let Err(e) = self.stdin.read_to_end(&mut buf) {
-        return Some(Err(e));
-      };
-
-      let something = match String::from_utf8(buf) {
-        Ok(something) => something,
-        Err(_) => return Some(Err(io::Error::new(io::ErrorKind::InvalidData, "invalid utf8 input"))),
-      };
-
-      self.words = something.split_whitespace().map(String::from).collect();
-      self.has_read = true;
+    if let Some(word) = self.words.pop_front() {
+      return Some(Ok(word));
     }
 
-    if self.index < self.words.len() {
-      let result = &self.words[self.index];
-      self.index += 1;
-      return Some(Ok(result.clone()));
+    if let Err(x) = self.read_words() {
+      return Some(Err(x));
+    }
+
+    if let Some(word) = self.words.pop_front() {
+      return Some(Ok(word));
     }
 
     return None;
@@ -61,35 +62,37 @@ struct Problem {
 }
 
 fn read_problem(words: &mut WordsIterator) -> Result<Option<Problem>> {
-  if let Some(Ok(problem_size)) = words.next() {
-    let problem_size: usize = problem_size.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid problem size"))?;
+  let problem_size: usize = match words.next() {
+    Some(Ok(x)) => x.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid problem size"))?,
+    Some(Err(x)) => Err(x)?,
+    None => return Ok(None),
+  };
 
-    if problem_size >= 3 {
-      let mut points: Vec<(f64, f64)> = Vec::new();
-
-      for _ in 0..problem_size {
-        let x: f64 = if let Some(Ok(x)) = words.next() {
-          x.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid coordinate"))?
-        } else {
-          return Err(io::Error::new(io::ErrorKind::InvalidData, "expected coordinate"));
-        };
-
-        let y: f64 = if let Some(Ok(y)) = words.next() {
-          y.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid coordinate"))?
-        } else {
-          return Err(io::Error::new(io::ErrorKind::InvalidData, "expected coordinate"));
-        };
-
-        points.push((x, y));
-      }
-
-      return Ok(Some(Problem {
-        points,
-      }));
-    }
+  if problem_size < 3 {
+    return Ok(None);
   }
 
-  return Ok(None);
+  let mut points: Vec<(f64, f64)> = Vec::new();
+
+  for _ in 0..problem_size {
+    let x: f64 = match words.next() {
+      Some(Ok(x)) => x.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid coordinate"))?,
+      Some(Err(x)) => Err(x)?,
+      None => Err(io::Error::new(io::ErrorKind::InvalidData, "expected coordinate"))?,
+    };
+
+    let y: f64 = match words.next() {
+      Some(Ok(x)) => x.parse().map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid coordinate"))?,
+      Some(Err(x)) => Err(x)?,
+      None => Err(io::Error::new(io::ErrorKind::InvalidData, "expected coordinate"))?,
+    };
+
+    points.push((x, y));
+  }
+
+  return Ok(Some(Problem {
+    points,
+  }));
 }
 
 fn print_solution(mut problem: Problem) {
@@ -156,8 +159,9 @@ fn area_of_triangle(ax: f64, ay: f64, bx: f64, by: f64, cx: f64, cy: f64) -> f64
 }
 
 fn main() -> Result<()> {
-  let mut stdin = io::stdin().lock();
-  let mut words = WordsIterator::new(&mut stdin);
+  let stdin = io::stdin();
+  let mut lines = stdin.lock().lines();
+  let mut words = WordsIterator::new(&mut lines);
 
   while let Some(problem) = read_problem(&mut words)? {
     print_solution(problem);
