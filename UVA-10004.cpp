b6267cb91b0eb1c48dedcdfccfc8fd627060f686
Kate Novak <kje@cochleoid.com>
2025-10-25 11:56:31 -0400

    UVA-10004 Another Potential Solution

--- UVA-10004.cpp
+++ UVA-10004.cpp
@@ -2,27 +2,55 @@
 //
 // g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE UVA-10004.cpp
 
+#include <algorithm>
+#include <deque>
 #include <iostream>
 #include <cstring>
+#include <set>
+#include <vector>
 
-int adj[200][200];
+struct Node {
+    std::set<size_t> neighbors;
+    bool color;
+};
 
-bool you_bi(size_t v) {
-    if (v < 2) {
-        return false;
-    } else if (v == 2) {
-        return true; // our graph is guaranteed to be connected
+bool you_bi(std::vector<Node> &nodes, std::set<std::pair<size_t, size_t>> &edges) {
+    std::set<size_t> seen;
+    std::deque<std::pair<bool, size_t>> queue;
+
+    seen.insert(0);
+    queue.push_back(std::make_pair(false, 0));
+
+    while (!queue.empty()) {
+        auto &item = queue.front();
+        bool color = !item.first;
+        size_t i = item.second;
+        queue.pop_front();
+
+        nodes[i].color = color;
+
+        std::cout << "node " << i << " has color " << nodes[i].color << std::endl;
+
+        for (size_t j : nodes[i].neighbors) {
+            if (seen.find(j) == seen.end()) { // if not seen
+                queue.push_back(std::make_pair(color, j));
+                seen.insert(j);
+            }
+
+            size_t a = i;
+            size_t b = j;
+
+            /*if (b < a) { std::swap(a, b); }
+            auto where = edges.find(std::make_pair(a, b));
+            if (where != edges.end()) {
+                edges.erase(where);
+            }*/
+        }
     }
 
-    for (size_t i = 0; i < v - 2; ++i) {
-        for (size_t j = i + 1; j < v - 1; ++j) {
-            if (adj[i][j]) {
-                for (size_t k = j + 1; k < v; ++k) {
-                    if (adj[k][j] && adj[i][k]) {
-                        return false;
-                    }
-                }
-            }
+    for (auto &edge : edges) {
+        if (nodes[edge.first].color == nodes[edge.second].color) {
+            return false;
         }
     }
 
@@ -31,23 +59,30 @@ bool you_bi(size_t v) {
 
 int main() {
     size_t v = 0;
-    size_t e = 0;
 
     while (std::cin >> v) {
         if (v == 0) {
             break;
         }
 
+        std::vector<Node> nodes(v);
+
+        size_t e = 0;
         if (std::cin >> e) {
-            std::memset(adj, 0, sizeof(adj));
+            std::set<std::pair<size_t, size_t>> edges;
 
             for (int i = 0; i < e; ++i) {
                 size_t a = 0, b = 0;
                 std::cin >> a >> b;
-                adj[a][b] = adj[b][a] = 1;
+
+                if (b < a) { std::swap(a, b); }
+                edges.insert(std::make_pair(a, b));
+
+                nodes[a].neighbors.insert(b);
+                nodes[b].neighbors.insert(a);
             }
 
-            std::cout << (you_bi(v) ? "BICOLORABLE." : "NOT BICOLORABLE.") << std::endl;
+            std::cout << (you_bi(nodes, edges) ? "BICOLORABLE." : "NOT BICOLORABLE.") << std::endl;
         }
     }
 
