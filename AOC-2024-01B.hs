71f94a367250c5aec4452d54e5b5b529c28fdd46
Kate Novak <kje@cochleoid.com>
2025-12-07 09:21:12 -0500

    Better AOC-2024-01B in Haskell

--- AOC-2024-01B.hs
+++ AOC-2024-01B.hs
@@ -1,3 +1,5 @@
+import Data.List (sort)
+
 to_ints :: [String] -> [Int]
 to_ints ws = map read ws
 
@@ -6,17 +8,28 @@ parse_lists ls = map to_ints (map words ls)
 left ws = map head ws
 right ws = map head $ map tail ws
 
+count_head_then_tail :: Ord a => a -> [a] -> (Int, [a])
+count_head_then_tail x [] = (0, [])
+count_head_then_tail x ys =
+  if x >= head ys then
+    let (count, rest) = count_head_then_tail x (tail ys)
+        increment = if x == head ys then 1 else 0
+    in (count + increment, rest)
+  else
+    (0, ys)
+
 calculate_similarity [] ys = 0
 calculate_similarity (x:xs) ys = 
-  let count = length $ filter (x ==) ys
-  in x * count + (calculate_similarity xs ys)
+  let (count_x, rest_xs) = count_head_then_tail x xs
+      (count_y, rest_ys) = count_head_then_tail x ys
+  in (x * (count_x + 1) * count_y) + (calculate_similarity rest_xs rest_ys)
 
 main :: IO ()
 main = do
   content <- getContents
   let lists = parse_lists (lines content)
-  let lefts = left lists
-  let rights = right lists
+  let lefts = sort $ left lists
+  let rights = sort $ right lists
 
   let score = calculate_similarity lefts rights
   print score
